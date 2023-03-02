1aaaa7f56beeeb88327b0a4bd5a65f7bb87c42b7
coraliney <carro_gryglas@hotmail.com>
2023-03-02 08:04:41 +0100

    fixing routes

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,5 +1,22 @@
-const express = require("express"); //tar detta ifrån?
+const express = require("express");
 const router = express.Router();
+const { userRoles } = require("../constants/users");
+const { isAuthenticated } = require("../middleware/authenticationMiddleware");
 
-const { UnauthenticatedError, UnauthorizedError } = require("../utils/errors");
-const jwt = require("jsonwebtoken");
+// const { UnauthenticatedError, UnauthorizedError } = require("../utils/errors"); - FUTURE.
+
+const {
+  createNewReview,
+  updateReviewByID,
+  deleteReviewById,
+  getReviewByID,
+  getAllReviews,
+} = require("../controller/reviewController");
+
+router.post("/", isAuthenticated, createNewReview);
+router.put("/:reviewId", isAuthenticated, updateReviewById);
+router.delete("/:reviewId", isAuthenticated, deleteReviewById);
+router.get("/:reviewId", getReviewById);
+router.get("/", getAllReviews);
+
+module.exports = router;
