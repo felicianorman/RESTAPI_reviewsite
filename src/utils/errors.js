00348bb8b8f72db5502783472306a856be71c8ec
coraliney <carro_gryglas@hotmail.com>
2023-03-02 10:53:37 +0100

    fixed errors in utils

---
+++ src/utils/errors.js
@@ -0,0 +1,60 @@
+const catchErrors = (fn) => {
+  return function (req, res, next) {
+    return fn(req, res, next).catch(next);
+  };
+};
+
+class CustomAPIError extends Error {
+  constructor(message) {
+    //VILKET ÄR MEDDELANDET? HÄNGER DET IHOP MED CONTROLLERS ERROR? DERAS MESSAGE?
+    super(message);
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+    this.name = "BadReqiestError"; //KAN MAN SKRIVA VAD SOM HELST HÄR?
+  }
+}
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+    this.name = "NotFoundError";
+  }
+}
+
+class UnauthenticatedError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+    this.name = "UnauthenticatedError";
+  }
+}
+
+class UnauthorizedError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = 403;
+    this.name = "UnautorizedError";
+  }
+}
+
+class ValidationError extends BadRequestError {
+  constructor(message, validationErrors) {
+    super(message);
+    this.validationErrors = validationErrors;
+  }
+}
+
+module.exports = {
+  catchErrors,
+  NotFoundError,
+  BadRequestError,
+  UnauthenticatedError,
+  UnauthorizedError,
+  ValidationError,
+};
