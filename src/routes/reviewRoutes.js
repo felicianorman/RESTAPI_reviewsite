2beb7eb2e330ab4356a9cb468224e271f1ac646d
coraliney <carro_gryglas@hotmail.com>
2023-03-02 08:19:06 +0100

    review update

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { userRoles } = require("../constants/users");
+const { userRoles } = require("../constants/users"); //DOESNT WORK - table or userRoles?
 const { isAuthenticated } = require("../middleware/authenticationMiddleware");
 
 // const { UnauthenticatedError, UnauthorizedError } = require("../utils/errors"); - FUTURE.
 
 const {
   createNewReview,
-  updateReviewByID,
+  updateReviewByID, //DOESNT WORK?
   deleteReviewById,
-  getReviewByID,
+  getReviewByID, //DOESNT WORK?
   getAllReviews,
 } = require("../controller/reviewController");
 
 router.post("/", isAuthenticated, createNewReview);
-router.put("/:reviewId", isAuthenticated, updateReviewById);
-router.delete("/:reviewId", isAuthenticated, deleteReviewById);
-router.get("/:reviewId", getReviewById);
+router.put("/", isAuthenticated, updateReviewById);
+router.delete("/", isAuthenticated, deleteReviewById);
+router.get("/", getReviewById);
 router.get("/", getAllReviews);
 
 module.exports = router;
