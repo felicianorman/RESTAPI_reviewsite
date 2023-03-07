988e8df25cdd9ec6deab28baed7930efaa1501af
maisahjuevesano <maisahu@live.se>
2023-03-07 15:07:45 +0100

    added updatecontroller and updateroute

--- src/controller/companyController.js
+++ src/controller/companyController.js
@@ -124,3 +124,45 @@ exports.createNewCompany = async (req, res) => {
     .setHeader('Location', `${req.protocol}://${req.headers.host}/api/v1/companies/${newCompanyId}`)
     .sendStatus(201)
 }
+
+exports.updateCompanyById = async (req, res) => {
+	const { name, adress, fk_city_id } = req.body;
+	const companyId = req.params.companyId;
+	try {
+	  if (req.user?.role !== userRoles.ADMIN) {
+		throw new UnauthorizedError("Unauthorized Access");
+	  }
+	  const [companyExists] = await sequelize.query(
+		`SELECT * FROM company WHERE id =$companyId;`,
+		{
+		  bind: {
+			companyId: companyId,
+		  },
+		}
+	  );
+  
+	  if (!companyExists || companyExists.length < 1) {
+		throw new BadRequestError("That company does not exists");
+	  }
+  
+	  const [updatedcompany] = await sequelize.query(
+		`UPDATE company SET name= $name, adress = $adress, fk_city_id=$fk_city_id
+		WHERE id = $companyId RETURNING *;`,
+		{
+		  bind: {
+			name: name,
+			adress: adress,
+			fk_city_id: fk_city_id,
+			companyId: companyId,
+		  },
+		}
+	  );
+  
+	  console.log(updatedcompany[0]);
+  
+	  return res.status(200).json(updatedcompany[0]);
+	} catch (error) {
+	  console.error(error);
+	  return res.status(500).json({ message: error.message });
+	}
+  };
--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { getAllCompanies, getCompanyById, deleteCompanyById, createNewCompany } = require('../controller/companyController')
+const { getAllCompanies, getCompanyById, deleteCompanyById, createNewCompany, updateCompanyById } = require('../controller/companyController')
 
 router.get('/', getAllCompanies)
 router.get('/:companyId', getCompanyById)
 router.delete('/:companyId', deleteCompanyById)
 router.post('/', createNewCompany)
+router.put('/:companyId', updateCompanyById);
 
 module.exports = router
+
+//lägga till isAuthenticated, authorizeRoles(userRoles.ADMIN)
