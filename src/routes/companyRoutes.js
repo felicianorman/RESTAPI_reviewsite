d12c6a9094896ce06dee1a8822f958d99748247c
maisahjuevesano <maisahu@live.se>
2023-03-07 10:30:41 +0100

    remove old files

--- src/routes/companyRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getAllCompanies, getCompanyById } = require('../controller/companyController')
-
-router.get('/', getAllCompanies)
-router.get('/:companyId', getCompanyById)
-
-
-module.exports = router
