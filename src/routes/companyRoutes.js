a04defb1df4c316dd4ff20d98a7a3c138ae2ccd5
maisahjuevesano <maisahu@live.se>
2023-03-07 15:21:38 +0100

    added isAuthenticated and authorizeRoles

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -1,13 +1,14 @@
-const express = require('express')
+const express = require('express');
+const { userRoles } = require('../constants/users');
 const router = express.Router()
 const { getAllCompanies, getCompanyById, deleteCompanyById, createNewCompany, updateCompanyById } = require('../controller/companyController')
 
+const { isAuthenticated, authorizeRoles} = require("../middleware/authenticationMiddleware");
+
 router.get('/', getAllCompanies)
 router.get('/:companyId', getCompanyById)
-router.delete('/:companyId', deleteCompanyById)
-router.post('/', createNewCompany)
-router.put('/:companyId', updateCompanyById);
+router.delete('/:companyId', isAuthenticated, authorizeRoles(userRoles.ADMIN), deleteCompanyById)
+router.post('/', isAuthenticated, authorizeRoles(userRoles.ADMIN), createNewCompany)
+router.put('/:companyId', isAuthenticated, authorizeRoles(userRoles.ADMIN), updateCompanyById);
 
 module.exports = router
-
-//lägga till isAuthenticated, authorizeRoles(userRoles.ADMIN)
