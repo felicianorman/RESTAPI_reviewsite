c07ee0c830607d39094ec799a96d75c06dea68b4
maisahjuevesano <maisahu@live.se>
2023-03-07 14:13:39 +0100

    added middleware

---
+++ src/middleware/validation/validationSchemas.js
@@ -0,0 +1,22 @@
+const { body } = require('express-validator')
+
+exports.registerSchema = [
+	body('email').isEmail().withMessage('You must provide a valid email address'),
+	body('password')
+		.not()
+		.isEmpty()
+		.isLength({ min: 8 })
+		.withMessage('You must provide a password that is at least 8 characters long'),
+]
+
+exports.loginSchema = [
+	body('email').isEmail().withMessage('You must provide a valid email address'),
+	body('password').not().isEmpty().withMessage('You must provide a password'),
+]
+
+
+
+
+//validering för update, behövs inte not empty
+//validering för skapa
+//validering för delete vem som har tillgång att radera
