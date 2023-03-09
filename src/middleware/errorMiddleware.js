1d6cb85d17e590637bfc646c1b9f2eb26a9e318b
maisahjuevesano <maisahu@live.se>
2023-03-09 10:11:30 +0100

    remove comment

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -15,24 +15,6 @@ exports.errorMiddleware = (error, req, res, next) => {
 
 	if (error instanceof ValidationError) customError.validatonErrors = error.validationErrors
 
-	/*
-	if (error.name === 'ValidationError') {
-		customError.validatonErrors = Object.values(error.errors).map((item) => item.message)
-
-		customError.statusCode = 400
-	}
-
-	// Reformats Mongoose error when a duplicate value is entered for a...
-	// ...field that has the "unique: true" validation
-	// prettier-ignore
-	if (error.code && error.code === 11000) {
-    customError.message = `Duplicate value entered for ${Object.keys(
-      error.keyValue
-    )} field, please choose another value`
-    customError.statusCode = 400
-  }
-	*/
-
 	if (error.name === 'CastError') {
 		customError.message = `No item found with id : ${error.value}`
 		customError.statusCode = 404
